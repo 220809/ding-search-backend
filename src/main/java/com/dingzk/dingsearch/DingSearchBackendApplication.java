6c83d08e471d5044fa7e00c3e93f97665d075f92
dingzk <110876210+220809@users.noreply.github.com>
2025-11-14 10:17:18 +0800

    SpringBoot 项目初始化. SpringBoot 版本 3.5.7.

---
+++ src/main/java/com/dingzk/dingsearch/DingSearchBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dingzk.dingsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DingSearchBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DingSearchBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dingzk/dingsearch/DingSearchBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dingzk.dingsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DingSearchBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
